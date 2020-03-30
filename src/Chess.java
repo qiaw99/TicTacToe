353df7f4377ebebf7c16a92bf253b0fb06dcb770
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-03-30 19:18:01 +0200

    Create Chess.java

---
+++ src/Chess.java
@@ -0,0 +1,11 @@
+package Chess;
+
+public class Chess {
+	String colorString;
+	int pos;
+	
+	public Chess(String colorString, int pos) {
+		this.colorString = colorString;
+		this.pos = pos;
+	}
+}
