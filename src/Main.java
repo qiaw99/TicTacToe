4c99491ae5e81004e4877e3e5e49def6a3c206a4
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-03-30 19:18:42 +0200

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package Chess;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			Play play = new Play();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
